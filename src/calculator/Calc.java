087144ff57d57a3350d59f017d20906040ce2a5d
Leo Guts <lenya.gutsev@mail.ru>
2023-06-09 21:46:47 +0300

    Чтение выражения из прописанной строки. Использование токена для разделения символов. Реализация сложения и вычитания.

---
+++ src/calculator/Calc.java
@@ -0,0 +1,33 @@
+package calculator;
+
+public class Calc {
+    private String[] tokens;
+    private int pos; // token position
+    public Calc(String expr) {
+        this.tokens = expr.split(" ");
+        this.pos = 0;
+    }
+
+    public static void main(String[] args) {
+        String expr = "1 + 2 - 3 + 4 - 5";
+
+        Calc calc = new Calc(expr);
+         double result = calc.parse();
+        System.out.println(expr + " = " + result);
+    }
+    private double parse() {
+        double first = Double.parseDouble(tokens[pos++]);
+        while (pos < tokens.length-1) {
+            String operator = tokens[pos];
+            if (!operator.equals("+") && !operator.equals("-")) {
+                break;
+            } else  { pos++; }
+            double second = Double.parseDouble(tokens[pos++]);
+            if (operator.equals("+")) {
+                first += second;
+            } else { first -= second; }
+        }
+
+        return first;
+    }
+}
