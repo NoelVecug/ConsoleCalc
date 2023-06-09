5f05b106e6674286cf45a8c4a6618ee9059563ce
Leo Guts <lenya.gutsev@mail.ru>
2023-06-09 22:29:59 +0300

    Добавлен метод определения скобок - factor. Определение символов и порядка их выполнения основаны на скрытой рекурсии.

--- src/calculator/Calc.java
+++ src/calculator/Calc.java
@@ -9,7 +9,7 @@ public class Calc {
     }
 
     public static void main(String[] args) {
-        String expr = "1 + 2 * 3 / 2 - 5";
+        String expr = "1 + 2 * 3 / ( 2 * 2 - 5 )";
 
         Calc calc = new Calc(expr);
         double result = calc.parse();
@@ -17,9 +17,9 @@ public class Calc {
         System.out.println(expr + " = " + result);
 
     }
-    private double parse() {
+    private double parse() { // проверка наличия сложения и вычитания
         double first = multiply();
-        while (pos < tokens.length-1) {
+        while (pos < tokens.length) {
             String operator = tokens[pos];
             if (!operator.equals("+") && !operator.equals("-")) {
                 break;
@@ -33,20 +33,41 @@ public class Calc {
 
         return first;
     }
-    private double multiply() {
-        double first = Double.parseDouble(tokens[pos++]);
-        while (pos < tokens.length-1) {
+    private double multiply() { // проверка наличия умножения и деления
+        double first = factor();
+        while (pos < tokens.length) {
             String operator = tokens[pos];
             if (!operator.equals("*") && !operator.equals("/")) {
                 break;
             } else  { pos++; }
 
-            double second = Double.parseDouble(tokens[pos++]);
+            double second = factor();
             if (operator.equals("*")) {
                 first *= second;
             } else { first /= second; }
         }
 
         return first;
+    }
+    private double factor() { // проверка наличия скобок
+        String next = tokens[pos];
+        double result;
+        if (next.equals("(")) {
+            pos++;
+            result = parse();
+            String closingBracket;
+            if (pos < tokens.length) {
+                closingBracket = tokens[pos];
+            } else {
+                throw new IllegalArgumentException("Unexpected end of expression");
+            }
+            if (closingBracket.equals(")")) {
+                pos++;
+                return result;
+            }
+            throw new IllegalArgumentException("')' expected but " + closingBracket + " found");
+        }
+        pos++;
+        return Double.parseDouble(next);
     }
 }
